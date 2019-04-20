e728fb262834cfbe9ca5827941fcf5c9d2fd1e86
Maina <kravchenko.marite@gmail.com>
2019-04-20 16:25:10 +0300

    создание скрипта для урока 1

--- Lesson-1/tempCodeRunnerFile.js
+++ Lesson-1/tempCodeRunnerFile.js
@@ -9,6 +9,7 @@ let appData = {
   savings: false
 };
 
+
 let question1 = prompt("Введите обязательную статью расходов в этом месяце"),
   question2 = prompt("Во сколько обойдется?");
 
@@ -20,5 +21,3 @@ question1 = prompt("Во сколько обойдется?");
 appData.expenses[question1] = question2;
 
 console.log(appData);
-
-alert (appData.budget/30);
