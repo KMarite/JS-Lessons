93e63282cdbcfc31dec7f35cded9aae6f84b1037
Maina <kravchenko.marite@gmail.com>
2019-04-25 10:58:17 +0300

    исправлены ошибки в main.js

--- Lesson-2/main.js
+++ Lesson-2/main.js
@@ -13,11 +13,11 @@ for (let i = 0; i < 2; i++) {
     let a = prompt("Введите обязательную статью расходов в этом месяце", ''),
         b = prompt("Во сколько обойдется?", '');
 
-    if ( (typeof(a))=== 'string' && (typeof(a)) != null && (typeof(b)) != null && a != '' && b != '' && a.length < 50) {
+    if ( (typeof(a))=== 'string' && a != null && b != null && a != '' && b != '' && a.length < 50) {
         console.log("done");
         appData.expenses[a] = b;
     } else {
-
+        i--;
     } 
 };
 
@@ -26,11 +26,11 @@ for (let i = 0; i < 2; i++) {
 //      i++;
 //     let a = prompt("Введите обязательную статью расходов в этом месяце", ''),
 //         b = prompt("Во сколько обойдется?", '');
-//      if ((typeof (a)) === 'string' && (typeof (a)) != null && (typeof (b)) != null && a != '' && b != '' && a.length < 50) {
+//      if ((typeof (a)) === 'string' && a != null && b != null && a != '' && b != '' && a.length < 50) {
 //         console.log("done");
 //         appData.expenses[a] = b;
 //      } else {
-
+//         i--;
 //      }
 //     };
 
@@ -39,11 +39,11 @@ for (let i = 0; i < 2; i++) {
 //     i++;
 //     let a = prompt("Введите обязательную статью расходов в этом месяце", ''),
 //         b = prompt("Во сколько обойдется?", '');
-//     if ((typeof (a)) === 'string' && (typeof (a)) != null && (typeof (b)) != null && a != '' && b != '' && a.length < 50) {
+//     if ((typeof (a)) === 'string' && a != null && b != null && a != '' && b != '' && a.length < 50) {
 //         console.log("done");
 //         appData.expenses[a] = b;
 //     } else {
-
+//         i--;
 //     }  
 // } while (i < 2);
 
